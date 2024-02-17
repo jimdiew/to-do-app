838e3f2744348edc59a46324239ab4fd669d8541
Jimena Diewiatajew <jimediew@gmail.com>
2024-02-16 23:02:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { ToDoWrapper } from "./components/ToDoWrapper"
+
+function App() {
+  return (
+    <>
+      <ToDoWrapper />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/EditToDoForm.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+export const EditToDoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    editTodo(value, task.id)
+    setValue("")
+  }
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Edit task"
+        onChange={e => setValue(e.target.value)}
+        value={value}
+      />
+      <button type="submit" className="todo-btn">
+        Edit Task
+      </button>
+    </form>
+  )
+}
